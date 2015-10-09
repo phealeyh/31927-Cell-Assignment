7368d261943107729d37cc26f56dae04704bc34e
phealeyh <tranceformas_101@hotmail.com>
2015-10-09 13:24:40 +1100

    First commit

--- CellAuto/Generation.cs
+++ CellAuto/Generation.cs
@@ -1,18 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace CellAuto
 {
-
     class Generation
     {
         private const int SIZE_GENERATION = 20;
         private Cells[] generations;
         private Int32 seed;
+
+        /* Generation Constructor
+         * -----------------------
+         * This will firstly generate the first
+         * generation based on the given seed.
+         * It will then go through generations 1-19
+         * and set those cells based on the previous set of cells generated from
+         * the for loop. It will initialise those cells based on the previous cells
+         * */
         public Generation(Int32 seed)
         {
             this.seed = seed;
@@ -29,13 +34,20 @@ namespace CellAuto
                     generations[i].setCells(generations[i - 1].getCells);
                 }
             }
-        } 
+        }
 
         public void printCells()
         {
-            for(int i = 0; i < SIZE_GENERATION; i++)
+            for (int i = 0; i < SIZE_GENERATION; i++)
             {
-                Console.Out.Write("gen  {0} ", i + 1);
+                if(i < 9)
+                {
+                    Console.Out.Write("gen   {0} ", i + 1);
+                }
+                else
+                {
+                    Console.Out.Write("gen  {0} ", i + 1);
+                }
                 generations[i].printCells();
             }
         }
@@ -45,11 +57,20 @@ namespace CellAuto
             get { return generations; }
         }
 
+
+        /* Method: getHash
+         * -----------------------
+         * This will firstly retrieve the final line's group of cells
+         *  and then loop through each element within that array to calculate
+         *  the hash value given by the states.
+         * */
+
+
         public int getHash()
         {
             int hashValue = 0;
             Cell[] finalLine = generations[SIZE_GENERATION - 1].getCells;
-            for(int i = 0; i < finalLine.Length; i++)
+            for (int i = 0; i < finalLine.Length; i++)
             {
                 hashValue += (i + 1) * (int)finalLine[i].getCurrentState;
             }
